ba8367452bc9e1af1173104f6e720bffe661b56f
Cristi Balu <cristi.s.balu@gmail.com>
2020-03-14 11:59:35 +0200

    added normal shoot

--- shoot.py
+++ shoot.py
@@ -1,13 +1,15 @@
 from login import login
 import requests
 
-urlTest = 'http://ic2020-fea-api.netrom.live/game/119/shoot'
+url = 'http://ic2020-fea-api.netrom.live/game/'
 
 def shootTest(x, y, token):
+  return shoot(x, y, "119", token)
+
+def shoot(x, y, joc, token):
   data = '{"x" : "' + x + '", "y" : "' + y + '"}'
-
-  response = requests.post(urlTest, data=data, headers={"Content-Type": "application/json", "Authorization": 'Bearer ' + token}).json()
-
+  response = requests.post(url + joc + "/shoot", data=data, headers={"Content-Type": "application/json", "Authorization": 'Bearer ' + token}).json()
+  
   return (response["code"])
 
 if __name__ == '__main__':
