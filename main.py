a75fd547c76e2851d39f2b409cd96bd8bbe39701
Cristi Balu <cristi.s.balu@gmail.com>
2020-03-14 11:16:55 +0200

    main.py initial

---
+++ main.py
@@ -0,0 +1,14 @@
+from probabilitati_initiale_cap import matriceInitiala
+
+cazuriPosibile = 84
+probabilitati = matriceInitiala()
+
+raspuns = 0
+
+if raspuns == 0:
+  cazuriPosibile -= 1
+  probabilitati[x][y] = 0
+  
+
+
+print(probabilitati)
